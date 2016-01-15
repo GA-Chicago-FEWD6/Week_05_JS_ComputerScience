f6b514d039ac7b3b2926e205ce77a1e784829359
saravogl <saravogl11@gmail.com>
2016-01-14 19:54:18 -0600

    Commit changes for Steve Harvey

---
+++ 09_variables_conditionals/solution_student/temp_converter/js/main.js
@@ -0,0 +1,22 @@
+var input = $('#temperature').val();
+
+$('#fahrenheit_to_celsius').on('click', function () {
+	input - 32 * 5/9;
+	var result = $('#result')
+
+};
+
+$('#celsius_to_fahrenheit').on('click', function () {
+	input + 32 * 9/5;
+};
+
+
+// user enters a value
+
+
+// user clicks F to clicks
+// get value of input
+// use formula of F to C to convert to C
+// display convresion in result 
+// 
+// user clicks C to F
