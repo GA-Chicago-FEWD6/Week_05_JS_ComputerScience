09fb51b69c7c98be3f5d62b14d993853e087d026
Hannah <hannah.demilta@gmail.com>
2016-01-14 19:54:21 -0600

    classwork

---
+++ 09_variables_conditionals/solution_student/temp_converter/js/main.js
@@ -0,0 +1,35 @@
+// enter value 
+$('#fahrenheit_to_celsius').on('click' function () {
+	var inputTemp = $('#temperature').val();
+	inputTemp = parseFloat(inputTemp);
+	
+
+	if (condition) {
+		(fahrenheit - 32) / 1.8
+	}
+});
+
+// user enters value
+// 
+// user clicks F to C
+//  initialVal = get value of input
+//  resultVal = use formula of F to C to convert initialVal to C
+//  
+//  if initialVal is blank
+//    display error
+//  else (if not blank)
+//    display resultVal in #result
+// 
+// user clicks C to F
+//  initialVal = get value of input
+//  resultVal = use formula of C to F to convert initialVal to F
+//  display resultVal in #result
+//  
+//  SUPER BONUS FUN ZONE
+//  display error if input is blank
+
+$('#celsius_to_fahrenheit').on('click' function () {
+	var inputTemp = $('#temperature').val();
+	inputTemp = parseFloat(inputTemp);
+
+});
