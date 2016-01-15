a99df7ba431897a2d2c8216ceaa980ac4f209df4
needleinthehaley <haleymsmith@gmail.com>
2016-01-14 19:54:21 -0600

    varcon in

---
+++ 09_variables_conditionals/solution_student/temp_converter/js/main.js
@@ -0,0 +1,14 @@
+$('#fahrenheit_to_celsius').on('click', function() {
+	var typedTemp = $('#temperature').val();
+	typedTemp = parseFloat(typedTemp);
+
+
+});
+
+$('#celsius_to_fahrenheit').on('click', function() {
+	var typedTemp = $('#temperature').val();
+	typedTemp = parseFloat(typedTemp);
+
+	
+});
+
