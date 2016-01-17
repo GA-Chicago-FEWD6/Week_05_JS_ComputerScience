b429b08cfabc6fb999704fad328b07534b9ea8ab
rykalin <alla@MacBookAir.local>
2016-01-16 21:14:41 -0600

    Assignment #5 Submit

--- 09_variables_conditionals/solution_student/temp_converter/js/main.js
+++ 09_variables_conditionals/solution_student/temp_converter/js/main.js
@@ -3,24 +3,10 @@
 //if CtoF clicked then take value and 
 // if no value entered show error message
 
-
-
-
-
 //user enters value
-
 //user clicks F to C
 //T(°C) = (T(°F) - 32) / 1.8
-$('#fahrenheit_to_celsius').on('click', function () {
-  var temp = $('#temperature').val();
 
-
-  if (temp == '') {
-     //display =
-     $('#result').html('Please enter value');
-    } else {temp = (temp-32)/1.8;}
-  $('#result').html(temp);
-});
 	//initialVal = get value of input
 	//resultVal = use formula F to C to convert initial val to C
 	
@@ -31,42 +17,51 @@ $('#fahrenheit_to_celsius').on('click', function () {
 
 //user clicks C to F
 //T(°F) = T(°C) × 1.8 + 32
-$('#celsius_to_fahrenheit').on('click', function () {
-  var temp = $('#temperature').val();
-  temp = parseFloat(temp);
-  temp = (temp*1.8)+32;
-  $('#result').html(temp);
-
-});
 
 //  initialVal = get val of input
 //  resultVal = use formula of C to F to convert initialVal to F 
 //  display resultVAl in #result 
 
-//(look up formula how to convert)
-
 //bonus
 //display error if input is blank
+//isnan (temp) is not equal to 
 
 
-// When the user clicks on #submit
-// $('#submit').on('click', function () {
-//   // get number 1 val (and store as var)
-//   var num1 = $('#num1').val();
-//   num1 = parseFloat(num1);
-//   // get number 2 val (and store as var)
-//   var num2 = $('#num2').val();
-//   num2 = parseFloat(num2);
-  
-//   //console.log(num1, num2);
-//    if (num1 == num2) {
-//      //display =
-//      $('#comparison').html('=');
-//      // if num 1 > numebr 2
-//     } else if (num1 > num2) {
-//     $('#comparison').html('>');
-//     } else if (num1 < num2) {
-//     $('#comparison').html('<');
-//     }
-// });
+$('#fahrenheit_to_celsius').on('click', function () {
+  var temp = $('#temperature').val();
+  temp = parseFloat(temp);
+  var convTemp = (temp - 32) / 1.8;
+  if (isNaN(temp)) {
+ 	console.log(isNaN(temp));
+ 	$('#result').html('');
+ 	$('#temperature').addClass('error');
+ 	$('.error-message').fadeIn('slow').addClass('error');
+  	} else {
+  	console.log(isNaN(temp));
+  	$('.error-message').hide();
+ 	$('#result').html(temp + ' °F = ' + convTemp + ' °C');
+ 	                 
+  }
+});
+
+
+$('#celsius_to_fahrenheit').on('click', function () {
+  var temp = $('#temperature').val();
+  temp = parseFloat(temp);
+  var convTemp = (temp * 1.8) + 32;
+  if (isNaN(temp)) {
+  	console.log(isNaN(temp));
+  	$('#result').html('');
+  	$('#temperature').addClass('error');
+ 	$('.error-message').fadeIn('slow').addClass('error');
+  	} else {
+  	console.log(isNaN(temp));
+  	$('.error-message').hide();
+ 	$('#result').html(temp + ' °C = ' + convTemp + ' °F');
+	}
+});
+
+
+
+
 
