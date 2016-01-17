c522a44a85e021e747e72c99064a5a318fb62ae2
CBrandess <carlybrandess@Carly-Brandesss-MacBook-Pro.local>
2016-01-17 00:23:12 -0600

    Assignment Final
    Would have liked to have made images fit the page and not repeat,
    however, I had some challenges in making it work in css. The
    background-repeat: no repeat was working, but nothing else I tried
    seemed to fit the image to the page.

--- 09_variables_conditionals/solution_student/temp_converter/js/main.js
+++ 09_variables_conditionals/solution_student/temp_converter/js/main.js
@@ -1,12 +1,30 @@
-var input = $('#temperature').val();
+// var input = $('#temperature').val();
 
 $('#fahrenheit_to_celsius').on('click', function () {
+	var input = $('#temperature').val();
+	changebackground(input);
 	var result = (input - 32) / 1.8;
 	$('#result').html(result);
 	});
 
 $('#celsius_to_fahrenheit').on('click', function () {
+	var input = $('#temperature').val();
 	var result = (1.8 * input) + 32;
 	$('#result').html(result);
+	changebackground(result);
+
 });
 
+
+function changebackground(temperature) {
+$('body').attr('class', '');
+if (temperature <= 20) {
+	$('body').addClass('freezing');
+}else if (temperature > 20 && temperature <= 60) {
+	$('body').addClass('bearabletemp');
+}else if (temperature > 60 && temperature <= 85) {
+	$('body').addClass('perfect');
+}else if (temperature > 85) {
+	$('body').addClass('hothothot');
+}
+}
